9e788728d8837d5b4df2d7d8a4ed2f4343046ac0
ninjadru <evil.little.dru@gmail.com>
2013-09-19 14:52:47 -0400

    Update robot.py

--- robot.py
+++ robot.py
@@ -10,37 +10,40 @@ def main():
     file = open('ana.log', 'a')
     usage = "usage: %prog [options] arg"
     parser = OptionParser(usage)
+    parser.add_option("-d", "--date", action="store_true", help="add a date stamp")
     parser.add_option("-t", "--todo", dest="todo", help="adds an entry for a todo")
     parser.add_option("-i", "--idea", dest="idea", help="adds an entry for an idea")
     parser.add_option("-e", "--event", dest="event", help="adds an entry for an event")
     parser.add_option("-l", "--list", action="store_true", dest="lst", help="list all entries")
     (options, args) = parser.parse_args()
-    if options.todo:
-	output = ("* " + options.todo)
+    if options.date:
 	date = datetime.datetime.now()
-    	datestamp = date.strftime('%m|%d|%Y ')
-    	file.write( datestamp + output + "\n" )
+        datestamp = date.strftime('%m|%d|%Y ')        
+        file.write( datestamp + "\n" + "--------" + "\n" )
+        file.close()
+
+    elif options.todo:
+	output = ("* " + options.todo)
+    	file.write( output + "\n" )
     	file.close()
 
     elif options.idea:
 	output = ("! " + options.idea)
-	date = datetime.datetime.now()
-    	datestamp = date.strftime('%m|%d|%Y ')
-    	file.write( datestamp + output + "\n" )
+    	file.write( output + "\n" )
     	file.close()
 
     elif options.event:
 	output = ("@ " + options.event)
-	date = datetime.datetime.now()
-    	datestamp = date.strftime('%m|%d|%Y ')
-    	file.write( datestamp + output + "\n" )
+    	file.write( output + "\n" )
     	file.close()
 
     elif options.lst:
         file = open("ana.log", "r")
-	lines= file.readlines() # Note that the content of line changes 
+	lines = file.readlines() # Note that the content of line changes 
+	
 	print ''.join(lines)
 	file.close()
+
 
 
 
